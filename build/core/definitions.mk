b63bfd159a610ef50394389c2aa291eec5fceb71
Zarathos30 <vale.rigni@gmail.com>
2026-09-26 09:57:28 +0200

    build: Package uwu_prebuilt_image radio partitions in target files
    uwu_prebuilt_image modules install radio firmware images to
    $(PRODUCT_OUT)/<partition>.img, but they are not registered in
    INSTALLED_RADIOIMAGE_TARGET. Target-files packaging therefore leaves
    RADIO/ empty and releasetools fails with:
    
      AssertionError: Failed to find <partition>.img
    
    Append AB_OTA_RADIO_PARTITIONS to INSTALLED_RADIOIMAGE_TARGET so the
    images are built before packaging and copied into RADIO/.
    
    Signed-off-by: Zarathos30 <vale.rigni@gmail.com>

--- build/core/definitions.mk
+++ build/core/definitions.mk
@@ -1,2 +1,6 @@
 # Include board/platform macros
 include vendor/uwu/build/core/utils.mk
+
+ifneq ($(strip $(AB_OTA_RADIO_PARTITIONS)),)
+INSTALLED_RADIOIMAGE_TARGET += $(addprefix $(PRODUCT_OUT)/,$(addsuffix .img,$(AB_OTA_RADIO_PARTITIONS)))
+endif
